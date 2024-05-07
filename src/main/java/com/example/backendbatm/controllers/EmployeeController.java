27bf0ab100d72d91f00946cbf8a7e6c4f9ea5d10
Kurs24 <maulanayasykur@gmail.com>
2024-05-07 15:31:30 +0700

    Employee CRUD

--- src/main/java/com/example/backendbatm/controllers/EmployeeController.java
+++ src/main/java/com/example/backendbatm/controllers/EmployeeController.java
@@ -1,5 +1,53 @@
 package com.example.backendbatm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.backendbatm.model.Employee;
+import com.example.backendbatm.repository.EmployeeRepository;
+
+@Controller
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
-    
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @GetMapping
+  public String index(Model model) {
+    model.addAttribute("employees", employeeRepository.findAll());
+    return "employee/index";
+  }
+
+  @GetMapping(value = {"form", "form/{id}"})
+  public String form(@PathVariable(required = false) Integer id, Model model) {
+    if (id != null) {
+      model.addAttribute("employee", employeeRepository.findById(id).get());
+    } else {
+      model.addAttribute("employee", new Employee());
+    }
+    return "employee/form";
+  }
+
+  @PostMapping("submit")
+  public String insert(Employee employee) {
+    try {
+      employeeRepository.save(employee);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return "redirect:/api/v1/employees";
+  }
+
+  @PostMapping("delete/{id}")
+  public String delete(@PathVariable("id") Integer id) {
+    try {
+      employeeRepository.deleteById(id);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return "redirect:/api/v1/employees";
+  }
 }

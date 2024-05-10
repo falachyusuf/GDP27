4cf9cca8aa28aaaa1de6a29107fd35c14d77d36e
HilmyThoriq <hilmythoriq09@gmail.com>
2024-05-10 16:14:29 +0700

    update EmployeeRestController

--- src/main/java/com/example/backendbatm/controllers/API/EmployeeRestController.java
+++ src/main/java/com/example/backendbatm/controllers/API/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,11 +46,21 @@ public class EmployeeRestController {
 
     @PutMapping("employee/{id}")
     public boolean edit(@PathVariable(required = true) Integer id, @RequestBody Employee employee) {
-        if (employeeRepository.findById(id).isPresent()) {
-            employee.setId(id);
-            employeeRepository.save(employee);
-            return true;
-        }else return false;
+        Employee employeeData = employeeRepository.findById(id).orElse(null);
 
+        if(employeeData == null){
+            return false;
+        }
+
+        if (employee.getEmail() != null && !employee.getEmail().equals("")) {
+            employeeData.setEmail(employee.getEmail());
+        }
+
+        if(employee.getName() != null && !employee.getName().equals("")){
+            employeeData.setName(employee.getName());
+        }
+
+        employeeRepository.save(employeeData);
+        return true;
     }
 }
